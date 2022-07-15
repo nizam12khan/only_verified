45f2630c73996c5f081295a746fd415077701389
Nizam <nizam12khan@gmail.com>
2022-07-15 11:28:08 +0000

    completed layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def newhelp(pagetitle='')
-    basetitle='Haqq'
-    pagetitle.empty? ? asetitle : pagetitle + " | " + basetitle 
+  def full_title(pagetitle='')
+    basetitle='Only verified'
+    pagetitle.empty? ? basetitle : pagetitle + " | " + basetitle 
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,29 +3,29 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @base="Haqq"
+    @base="Only verified"
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "home | #{@base}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "help | #{@base}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base}"
   end
 
   test "should get c" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base}"
   end
